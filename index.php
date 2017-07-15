2d82ef275eeec940831e719ebcfeaa24e91c48c6
leemem <leemem@gmail.com>
2017-07-15 15:31:35 +0800

    add qrcode generater

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>test</title>
+		<title>工具集 - 李茂琦</title>
 	</head>
 	<body style="margin: 0px; overflow: hidden;">
+		<h3>我的工具集</h3>
 		<ul>
 			<li><a href="/toolkits/computeDate.php">计算日期</a></li>
+			<li><a href="/toolkits/qrcode/qrcodeGenerate.php">计算日期</a></li>
 		</ul>
 	</body>
 </html>
---
+++ toolkits/qrcode/qrcodeGenerate.php
@@ -0,0 +1,46 @@
+<?php 
+reqire "../../vender/phparcode/qrlib.php";
+$hasfile = false;
+if(!empty($_POST)){
+	$content = isset($_POST["content"])?$_POST["content"]:"";
+	$filename = md5($content).".png";
+	$fullfilename = dirname(__FILE__) . "/output/" . $filename;
+	QRcode::png($content, $fullfilename);
+	if(is_file($fullfilename)){
+		$hasfile = true;
+	}
+}
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>在线生成二维码</title>
+		<style type="text/css">
+			.container{
+				width:960px;
+				margin: 0 auto;
+			}
+		</style>
+	</head>
+	<body style="margin: 0px; overflow: hidden;">
+		<div class="container">
+			<form action="/toolkits/qrcode/qrcodeGenerage.php" method="post">
+				<div>提交你要转换为二维码的内容</div>
+				<textarea style="width:100%;height:100px;" name="content"></textarea> <br /> <br />
+				<button type="submit">提交</button>
+			</form>
+
+			<?php
+				if($hasfile){
+			?>
+			<div style="color:green;">二维码生成成功，二维码生成如下：</div> <br />
+			<div>
+				<img src="/toolkits/qrcode/output/<?php echo $filename;?>" />
+			</div>
+			<?php
+				}
+			?>
+		</div>
+	</body>
+</html>
